32b8546f6f2995031229a4607764397f64684bfc
Orlando Suarez <corxosz@gmail.com>
2022-10-25 00:10:29 -0500

    ajustes mayores en header, footer y catalogo

---
+++ js/ceo.js
@@ -0,0 +1,10 @@
+// Servicios
+const catalagoCeo = [
+    {id:"blitz", nombre: "Blitz"},
+    {id:"galaxy", nombre: "Galaxy"},
+    {id:"kuga", nombre: "Kuga"},
+    {id:"seneca", nombre: "Seneca"},
+    {id:"stratos", nombre: "Stratos"},
+    {id:"work", nombre: "Work"},
+    {id:"x2", nombre: "X2"},
+];
